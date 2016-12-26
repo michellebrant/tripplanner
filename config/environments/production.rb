f57713ca67219bc4547ae64599719ce496e6c7f8
Michelle Brant <michellebrant>
2016-12-25 20:39:02 -0500

    final final heroky

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'https://damp-chamber-94000.herokuapp.com/users/sign_in'}
+  config.action_mailer.default_url_options = { host: 'https://damp-chamber-94000.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings ={
